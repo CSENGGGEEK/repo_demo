c7eef94ae548da15e5f2ffed10f5b1cf880924bb
Anant Chitranshi <anantchitranshi001@gmail.com>
2024-06-05 06:33:15 +0000

    Third Commit

--- helloworld.c
+++ helloworld.c
@@ -1,2 +1,8 @@
 /* Hey do you actually wat that I should write that C program ? */
+<<<<<<< Updated upstream
 /* I am creating a merge conflict */
+=======
+
+
+/* I dont want any conflict atleast */
+>>>>>>> Stashed changes
